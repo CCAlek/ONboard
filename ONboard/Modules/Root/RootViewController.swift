bdd29c2b9e181b8b20ad5ef1c75f2410fc25564b
Semen Semenov <semen.semenov@skyeng.ru>
2023-02-17 19:51:26 +0300

    Project configuration

---
+++ ONboard/Modules/Root/RootViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+}
+
